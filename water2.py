5aefb73ce8a7a17ce5c9aa49b39591eda303f093
MakkerJulian <juliankloosterhuis@gmail.com>
2023-03-30 14:59:54 +0200

    Inclusief boot

--- water2.py
+++ water2.py
@@ -1,5 +1,5 @@
 import pygame
-
+from time import sleep
 pygame.init()
 
 win = pygame.display.set_mode((1920,1080))
@@ -9,24 +9,37 @@ pygame.display.set_caption("Water Simulator")
 y=600
 width=20
 height=20
-vel=10
-
+vel=5
+sy=0
 
 image= pygame.image.load('noordpolderzijl.jpg')
 def background(image):
     size=pygame.transform.scale(image,(1920,1080))
     win.blit(size,(0,0))
 
-'''boat=pygame.image.load('boot.png')
+boat=pygame.image.load('boot.png')
 def add_boat(boat):
     size=pygame.transform.scale(boat,(600,200))
-    win.blit(size,(1920/2,1080/2))
-'''
+    win.blit(size,(600,y-50))
 
 water= pygame.image.load('water.png')
 def polder(image):
     size=pygame.transform.scale(image,(1920,1080))
     win.blit(size,(0,y))
+
+schuif=pygame.image.load('schuif.png')
+def add_schuif(schuif):
+    size=pygame.transform.scale(schuif,(100,150))
+    win.blit(size,(1150,y-sy))
+
+mnr=1
+motor=pygame.image.load('motor.png')
+greenmotor=pygame.image.load('motorgeel.png')
+def add_motor(motor):
+    size=pygame.transform.scale(motor,(100,100))
+    win.blit(size,(700,y-50))
+
+
 run=True
 
 while run:
@@ -47,10 +60,34 @@ while run:
     if keys[pygame.K_q]:
         pygame.quit()
 
+    if keys[pygame.K_w]:
+        if sy+vel== 60:
+            sy=sy
+        else:
+            sy+=vel
+    
+    if keys[pygame.K_s]:
+        if sy-vel == -20:
+            sy=sy
+        else: 
+            sy-=vel
+
+    if keys[pygame.K_SPACE]:
+        if mnr ==1:
+            mnr=0
+            sleep(0.1)
+        elif mnr==0:
+            mnr=1 
+            sleep(0.1)
 
     background(image)
     polder(water)
-
+    add_boat(boat)
+    add_schuif(schuif)
+    if mnr==1:
+        add_motor(motor)
+    elif mnr==0:
+        add_motor(greenmotor)
     pygame.display.update()
 
 pygame.quit()
