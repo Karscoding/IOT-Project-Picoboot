4025fe7353db29280ddd66ddd08b9e3b86223005
MakkerJulian <juliankloosterhuis@gmail.com>
2023-04-18 11:59:19 +0200

    automatische simulatie

--- water2.py
+++ water2.py
@@ -1,6 +1,7 @@
 import pygame
 from time import sleep
-
+import threading
+import random
 pygame.init()
 info = pygame.display.Info()
 SIZE = info.current_w, info.current_h
@@ -9,11 +10,12 @@ SIZE = info.current_w, info.current_h
 win = pygame.display.set_mode(SIZE)
 
 pygame.display.set_caption("Water Simulator")
-x=600
-y=600
+
+x=500
+y=400
 
 vel=5
-sy=0
+
 
 run=True
 
@@ -26,7 +28,7 @@ def background(image):
 zand=pygame.image.load('zand.png')
 def add_zand(zand):
     size=pygame.transform.scale(zand,(1920,200))
-    win.blit(size,(0,800))
+    win.blit(size,(0,700))
 
 boat=pygame.image.load('boot.png')
 def add_boat(boat):
@@ -40,8 +42,13 @@ def polder(image):
 
 schuif=pygame.image.load('schuif.png')
 def add_schuif(schuif):
+    global y
     size=pygame.transform.scale(schuif,(100,150))
-    win.blit(size,(x+550,y-sy))
+    if y>575:
+        sy=y-(y-575)
+    else:
+        sy=y
+    win.blit(size,(x+550,sy))
 
 mnr=1
 motor=pygame.image.load('motor.png')
@@ -56,72 +63,118 @@ WHITE=(255,255,255)
 
 run=True
 
-while run:
-    pygame.time.delay(10)
+def runengine():
+    global y
+    global run
+    global mnr
+    global x
 
-    for event in pygame.event.get():
-            if event.type== pygame.quit:
+    while run:
+        pygame.time.delay(10)
+
+        for event in pygame.event.get():
+                if event.type== pygame.QUIT:
+                    run=False
+            
+        background(image)
+        add_zand(zand)
+        polder(water)
+        add_boat(boat)
+        add_schuif(schuif)
+        if mnr==1:
+            add_motor(motor)
+        elif mnr==0:
+            add_motor(greenmotor)
+                    
+        # Draw black box around "esp" text
+        font = pygame.font.SysFont(None, 25)
+        text = font.render("Computer/ESP", True, WHITE)
+        pygame.draw.rect(win, BLACK, (x+300, y-20, 150, 50))
+        win.blit(text, (x+300, y))    
+
+        pygame.display.update()
+
+def adjusttide():
+     global y
+     global run
+     i=0
+     wy= random.randint(400,600)
+     if abs(y-wy)<=50:
+        if y-wy<0:
+            wy+=50
+        else: 
+            wy-=50
+
+     while run:
+        if y!=wy:
+            if y-wy<0:
+                y+=1
+                sleep(0.02)
+            elif y-wy>0:
+                y-=1
+                sleep(0.02)
+        else:
+            i+=1
+            if i == 6:
                 run=False
-        
-    keys=pygame.key.get_pressed()
-
-    if keys[pygame.K_UP] and y >500:
-            y-=vel
-
-    if keys[pygame.K_DOWN] and y < 700:
-            y+=vel
-        
-    if keys[pygame.K_q]:
-            pygame.quit()
-
-    if mnr==0:
-            if keys[pygame.K_w]:
-                if sy+vel== 60:
-                    sy=sy
+            wy= random.randint(400,600)
+            if abs(y-wy)<=50:
+                if y-wy<0:
+                    wy+=50
                 else:
-                    sy+=vel
-            
-            if keys[pygame.K_s]:
-                if sy-vel == -20:
-                    sy=sy
-                else: 
-                    sy-=vel
+                    if wy-50 <=400:
+                        wy=400 
+                    else:
+                        wy-=50
+            sleep(4)
 
-    if keys[pygame.K_RIGHT]:
-                if x+vel==1000:
-                    x=x
-                else:
-                    x+=vel
+
+thread1=threading.Thread(target=runengine)
+thread1.start()
+thread2 = threading.Thread(target=adjusttide)
+thread2.start()
+
+
+'''keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_UP] and y >500:
+                y-=vel
+
+        if keys[pygame.K_DOWN] and y < 700:
+                y+=vel
             
-    if keys[pygame.K_LEFT]:
-                if x+vel==100:
-                    x=x
-                else:
-                    x-=vel
-            
-    if keys[pygame.K_SPACE]:
-            if mnr ==1:
-                mnr=0
-                sleep(0.1)
-            elif mnr==0:
-                mnr=1 
-                sleep(0.1)
-        
-    background(image)
-    add_zand(zand)
-    polder(water)
-    add_boat(boat)
-    add_schuif(schuif)
-    if mnr==1:
-        add_motor(motor)
-    elif mnr==0:
-        add_motor(greenmotor)
+        if keys[pygame.K_q]:
+                pygame.quit()
+
+        if mnr==0:
+                if keys[pygame.K_w]:
+                    if sy+vel== 60:
+                        sy=sy
+                    else:
+                        sy+=vel
                 
-    # Draw black box around "esp" text
-    font = pygame.font.SysFont(None, 25)
-    text = font.render("Computer/ESP", True, WHITE)
-    pygame.draw.rect(win, BLACK, (x+300, y-20, 150, 50))
-    win.blit(text, (x+300, y))    
+                if keys[pygame.K_s]:
+                    if sy-vel == -20:
+                        sy=sy
+                    else: 
+                        sy-=vel
 
-    pygame.display.update()
-
+        if keys[pygame.K_RIGHT]:
+                    if x+vel==1000:
+                        x=x
+                    else:
+                        x+=vel
+                
+        if keys[pygame.K_LEFT]:
+                    if x+vel==100:
+                        x=x
+                    else:
+                        x-=vel
+                
+        if keys[pygame.K_SPACE]:
+                if mnr ==1:
+                    mnr=0
+                    sleep(0.1)
+                elif mnr==0:
+                    mnr=1 
+                    sleep(0.1)'''
