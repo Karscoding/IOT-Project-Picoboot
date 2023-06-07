059408b8fc79dae9b5d38c01a14c4a27d8b5729d
J0hn-Rick <128607362+J0hn-Rick@users.noreply.github.com>
2023-06-07 13:43:33 +0200

    Update panel_grafiek.py
    update grafiek

--- panel_grafiek.py
+++ panel_grafiek.py
@@ -7,28 +7,24 @@ from databasevuller import app, Afstand
 style.use('fivethirtyeight')
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(1, 1, 1)
 
 def run(dag=0):
-    distlist=[]
+    distlist = []
     
     with app.app_context(): 
         for x in Afstand.query.all():
-            distlist.append((x.afstand,x.nap))
-    print(distlist)
+            distlist.append((x.afstand, x.nap))
+    
+    return distlist
 
 def animate(i):
-    graph_data = 'dishlist'
-    lines = graph_data.split('\n')
-    xs = []
-    ys = []
-    for line in lines:
-        if len(line) > 1:
-            x, y = line.split(',')
-            xs.append(float(x))
-            ys.append(float(y))
-    ax1.clear()
-    ax1.plot(xs, ys)
+    data = run()
+    if data:
+        xs = [x[0] for x in data]
+        ys = [x[1] for x in data]
+        ax1.clear()
+        ax1.plot(xs, ys)
 
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
+ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
